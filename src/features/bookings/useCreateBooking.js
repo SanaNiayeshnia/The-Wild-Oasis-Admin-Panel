6981ae89d3e626be44f30fb2a6436abea8d0788b
Sana Niayeshnia <niayeshnia1381@gmail.com>
2024-07-04 14:07:52 +0330

    fixing searchparams bugs

--- src/features/bookings/useCreateBooking.js
+++ src/features/bookings/useCreateBooking.js
@@ -2,9 +2,11 @@ import { useMutation, useQueries, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { createBooking } from "../../services/apiBookings";
 import { useGeneralContext } from "../../contexts/GeneralContext";
+import { useSearchParams } from "react-router-dom";
 
 function useCreateBooking() {
   const { showModal, handleCloseModal } = useGeneralContext();
+  const [searchParams, setSearchParams] = useSearchParams();
   const queryClient = useQueryClient();
   const { mutate, isPending } = useMutation({
     mutationKey: ["bookings"],
@@ -12,6 +14,7 @@ function useCreateBooking() {
     onError: (err) => toast.error(err.message),
     onSuccess: (data) => {
       toast.success(`Booking ${data?.id} has been created successfully!`);
+      setSearchParams({});
       queryClient.invalidateQueries("bookings");
       queryClient.invalidateQueries({
         queryKey: [`today-activity`],
